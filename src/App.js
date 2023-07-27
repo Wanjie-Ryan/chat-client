f9a06c17108f03b0d8b45bfc8980ea50b8cc01eb
Ryan <ryanwanjiedistortion@gmail.com>
2023-07-27 15:05:38 +0300

    chat-client

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import IO from '../../'
+
+
+
+function App() {
+  return (
+    <div>App</div>
+  )
+}
+
+export default App
---
+++ src/components/IO/io.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+
+
+
+
+
+function Io() {
+
+
+
+  return (
+
+
+    <>
+
+
+
+    
+    
+    
+    
+    </>
+
+
+  )
+}
+
+export default Io
