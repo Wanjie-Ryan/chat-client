2a6441b136ce25881df3fdc1cb08b473999c38be
Ryan <ryanwanjiedistortion@gmail.com>
2023-07-29 07:20:35 +0300

    sockets-client

--- src/components/Sockets/chat.jsx
+++ src/components/Sockets/chat.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './chat.css'
 
 
 //passing data from io as props to chat component
-function chat({socket, username, room}) {
+function Chat({socket, username, room}) {
 
 
+    const [currentMessage, setCurrentMessage] = useState()
+
+    const handleMessage =(e)=>{
+
+        setCurrentMessage(e.target.value)
+    }
+
+    const sendMessage =()=>{
+
+        if(currentMessage !==''){
+            
+
+
+        }
+    }
 
   return (
 
@@ -13,7 +28,28 @@ function chat({socket, username, room}) {
     <>
 
     
-        <div>chat</div>
+       <div className='chat-header'>
+
+        <p>Live Chat</p>
+
+
+
+       </div>
+
+       <div className="chat-body">
+
+
+
+       </div>
+
+       <div className="chat-footer">
+
+        <input type="text" onChange ={handleMessage} value={currentMessage} placeholder="Type a message here..."/>
+        <button onClick ={sendMessage}>&#9658;</button>
+
+
+
+       </div>
 
 
 
@@ -25,4 +61,4 @@ function chat({socket, username, room}) {
   )
 }
 
-export default chat
+export default Chat
